c48e2f79d441815b8d94d0203ca80504c27802ef
zhouzhezz <zorichzorich@gmail.com>
2021-07-12 18:26:37 -0400

    rename scripts

--- bin/run_benchmarks_short.sh
+++ bin/run_benchmarks_short.sh
@@ -4,5 +4,9 @@ export LD_LIBRARY_PATH=`opam var z3:lib`:"$LD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH=`opam var z3:lib`:$DYLD_LIBRARY_PATH
 
 config=config/standard.config
-python3 evaluation_tool.py table $config
-python3 evaluation_tool.py figure $config
+tool=bin/evaluation_tool.py
+python3 $tool consistent $config
+python3 $tool weakening $config -s
+
+python3 $tool diff $config
+python3 $tool count $config -s
--- bin/visualize_prebuilt_result.sh
+++ bin/visualize_prebuilt_result.sh
@@ -4,5 +4,6 @@ export LD_LIBRARY_PATH=`opam var z3:lib`:"$LD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH=`opam var z3:lib`:$DYLD_LIBRARY_PATH
 
 config=config/prebuilt.config
-python3 evaluation_tool.py table $config
-python3 evaluation_tool.py figure $config
+tool=bin/evaluation_tool.py
+python3 $tool table $config
+python3 $tool figure $config
