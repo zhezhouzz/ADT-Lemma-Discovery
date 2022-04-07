db4208288ca10cd9545c20c89e386a992dc370eb
zhezhouzz <zorichzorich@gmail.com>
2022-04-06 23:58:02 -0400

    remove old

--- frontend_old/driver/frontend.ml
+++
@@ -1,8 +0,0 @@
-let tool_name = "ocamlc"
-let ppf = Format.err_formatter
-
-let parse ~sourcefile =
-  let structure = Pparse.parse_implementation ~tool_name ppf sourcefile in
-  let _ = Printast.structure 0 Format.std_formatter structure in
-  structure
-;;
--- frontend_old/driver/frontend.mli
+++
@@ -1,1 +0,0 @@
-val parse: sourcefile:string -> Parsetree.structure
