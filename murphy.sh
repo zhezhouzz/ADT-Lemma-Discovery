142173253a20a920985375de7baa5544e7d4812d
zhouzhezz <zorichzorich@gmail.com>
2022-04-09 12:13:01 -0400

    update script

--- murphy.sh
+++ murphy.sh
@@ -3,18 +3,18 @@ host_dir=".elrond"
 name=$1
 num=$2
 if [ "$3" = "to-murphy" ]; then
-	dune exec -- main/main.exe infer consistent data/$name.ml data/${name}_assertion$num.ml $name$num.elrond >.out
+	dune exec -- main/main.exe infer consistent data/$name.ml data/${name}_assertion$num.ml $name$num.elrond
 	cp _$name$num.elrond/_beforeweakening.json $host_dir/$name$num.elrond_beforeweakening.json
 fi
 
 if [ "$3" = "weakening" ]; then
 	mkdir _$name$num.elrond
 	cp $host_dir/$name$num.elrond_beforeweakening.json _$name$num.elrond/_beforeweakening.json
-	dune exec -- main/main.exe infer weakening $name$num.elrond > $name$num.elrond.out
+	dune exec -- main/main.exe infer weakening $name$num.elrond
 fi
 
 if [ "$3" = "weakening-murphy" ]; then
 	mkdir _$name$num.murphy
 	cp $host_dir/$name$num.elrond_beforeweakening.json _$name$num.murphy/_beforeweakening.json
-	dune exec -- main/main.exe infer weakening-murphy data/$name.ml data/${name}_assertion$num.ml $host_dir/_$name$num.elrond.spectable $host_dir/_$name$num.elrond.alpha.murphy $name$num.murphy > $name$num.murphy.out
+	dune exec -- main/main.exe infer weakening-murphy data/$name.ml data/${name}_assertion$num.ml $host_dir/_$name$num.elrond.spectable $host_dir/_$name$num.elrond.alpha.murphy $name$num.murphy
 fi
