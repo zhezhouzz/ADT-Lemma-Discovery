bfd7534232f459238e07c084d662872816ea087d
zhouzhezz <zorichzorich@gmail.com>
2022-04-09 01:36:04 -0400

    fix

--- murphy.sh
+++ murphy.sh
@@ -8,13 +8,13 @@ if [ "$3" = "to-murphy" ]; then
 fi
 
 if [ "$3" = "weakening" ]; then
-	mkdir $name$num.elrond
+	mkdir _$name$num.elrond
 	cp $host_dir/$name$num.elrond_beforeweakening.json _$name$num.elrond/_beforeweakening.json
 	dune exec -- main/main.exe infer weakening $name$num.elrond > $name$num.elrond.out
 fi
 
 if [ "$3" = "weakening-murphy" ]; then
-	mkdir $name$num.murphy
+	mkdir _$name$num.murphy
 	cp $host_dir/$name$num.elrond_beforeweakening.json _$name$num.murphy/_beforeweakening.json
 	dune exec -- main/main.exe infer weakening-murphy data/$name.ml data/${name}_assertion$num.ml $host_dir/_$name$num.elrond.spectable $host_dir/_$name$num.elrond.alpha.murphy $name$num.murphy > $name$num.murphy.out
 fi
